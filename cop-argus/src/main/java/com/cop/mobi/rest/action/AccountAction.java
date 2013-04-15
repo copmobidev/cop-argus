5c18a81a438e50c5b43a903716fab345e3d70f91
chris.liu <lynx.chrisliu@gmail.com>
2013-04-15 23:58:01 +0800

    新增头像上传模块

--- cop-argus/src/main/java/com/cop/mobi/rest/action/AccountAction.java
+++ cop-argus/src/main/java/com/cop/mobi/rest/action/AccountAction.java
@@ -1,5 +1,13 @@
 package com.cop.mobi.rest.action;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
+
+import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -7,12 +15,17 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import org.apache.commons.io.IOUtils;
+import org.jboss.resteasy.plugins.providers.multipart.InputPart;
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
+
 import com.cop.mobi.account.entity.User;
 import com.cop.mobi.account.service.AccountService;
 import com.cop.mobi.common.Result;
 import com.cop.mobi.common.Result.ResultStatus;
 import com.cop.mobi.mycar.entity.MyCar;
 import com.cop.mobi.rest.core.AbstractAction;
+import com.cop.mobi.rest.core.MD5Util;
 import com.cop.mobi.rest.core.SpringApplicationContext;
 
 /**
@@ -24,6 +37,8 @@ import com.cop.mobi.rest.core.SpringApplicationContext;
 @Produces("application/json;charset=UTF-8")
 public class AccountAction extends AbstractAction {
 	private static final String Tag = "AccountAction";
+
+	private static final String PROFILE_UPLOADED_PATH = "d:/data/resources/profile/";
 
 	private static AccountService accountService;
 
@@ -83,8 +98,47 @@ public class AccountAction extends AbstractAction {
 	}
 
 	@POST
+	@Path("/uploadprofile")
+	@Consumes("multipart/form-data")
+	public Response uploadProfile(MultipartFormDataInput input) {
+		Map<String, List<InputPart>> uploadForm = input.getFormDataMap();
+		List<InputPart> inputParts = uploadForm.get("uploadedFile");
+		for (InputPart inputPart : inputParts) {
+			try {
+				// convert the uploaded file to inputstream
+				InputStream inputStream = inputPart.getBody(InputStream.class,
+						null);
+				byte[] data = IOUtils.toByteArray(inputStream);
+				String fileName = PROFILE_UPLOADED_PATH + MD5Util.digest(data) + ".png";
+				// constructs upload file path
+				writeFile(data, fileName);
+			} catch (IOException e) {
+				log.error(
+						String.format("%s:%s", Tag, "uploadprofile exception"),
+						e);
+			}
+		}
+
+		return Response.status(Status.OK)
+				.entity("uploadFile is called, Uploaded file name").build();
+	}
+
+	@POST
 	@Path("/freeze")
 	public Response freeze() {
 		return null;
 	}
+
+	private void writeFile(byte[] content, String filename) throws IOException {
+		File file = new File(filename);
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+		FileOutputStream fop = new FileOutputStream(file);
+		fop.write(content);
+		fop.flush();
+		fop.close();
+
+	}
+
 }
