66058f1613df93a5498576f23f2fdb564f51070a
chris <chris@lynx>
2013-08-01 15:35:54 +0800

    split project into diff git repository

--- mvc-demo/deploy.sh
+++
@@ -1,3 +0,0 @@
-rm -r /home/chris/apache-tomcat-7/webapp/argus.war
-rm -r /home/chris/apache-tomcat-7/webapp/argus
-cp target/argus.war /home/chris/apache-tomcat-7/webapp/
--- mvc-demo/src/test/java/com/cop/argus/test/a/A.java
+++
@@ -1,5 +0,0 @@
-package com.cop.argus.test.a;
-
-public abstract class A {
-
-}
